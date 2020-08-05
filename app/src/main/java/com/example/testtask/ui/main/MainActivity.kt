26c87173581ab28c854090cbd8d88dc4d9582c5d
afanasiefa <katya.afanasiefa.vin@gmail.com>
2020-08-06 00:37:19 +0300

    task is completed:)

--- app/src/main/java/com/example/testtask/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/testtask/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.testtask.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.testtask.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        val navController = this.findNavController(R.id.hostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.hostFragment)
+        return navController.navigateUp()
     }
 }
